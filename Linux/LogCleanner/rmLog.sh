3685d25bf298a3094cc89529b2d8f2f7846d749d
Fuxx-1 <583742849@qq.com>
2022-07-14 16:32:20 +0800

    2022-07-14

--- Linux/LogCleanner/rmLog.sh
+++ Linux/LogCleanner/rmLog.sh
@@ -3,3 +3,4 @@ echo -e "rmOut:" `rm -rf *.log` "\n" `date "+%Y-%m-%d %H:%M:%S"` '|' "[*.log]Cle
 echo -e "rmOut:" `rm -rf /MySoftWare/mailServer/docker-data/dms/mail-logs` "\n" `date "+%Y-%m-%d %H:%M:%S"` '|' "[mailServerLog]Clean Over\n"
 echo -e "\ndiskSituation\n"
 df -H
+reboot
